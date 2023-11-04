bbe922270ad26482722a9daa069c910657f0fd32
Mohamed Sami <srmf2025@gmail.com>
2023-11-04 22:58:50 +0530

    fix: user profile picture

--- web/src/pages/[username]/[service].tsx
+++ web/src/pages/[username]/[service].tsx
@@ -108,7 +108,7 @@ export default function Service({
                       />
                     </div>
                     <Avatar>
-                      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                      <AvatarImage src={user.picture} alt="@shadcn" />
                       <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
                   </div>
