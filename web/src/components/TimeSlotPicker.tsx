5f86895be318d0f06c6a5ec781c9df9a59489486
Mohamed Sami <srmf2025@gmail.com>
2023-11-04 22:03:23 +0530

    feat: confirmation flow

--- web/src/components/TimeSlotPicker.tsx
+++ web/src/components/TimeSlotPicker.tsx
@@ -3,54 +3,39 @@ import React, { useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface TimeSlotPickerProps {
-  startTime: string;
-  endTime: string;
-  timeInterval: number;
+  slots: string[];
   selected?: string;
   onSelect: (time: string) => void;
 }
 
 const TimeSlotPicker: React.FC<TimeSlotPickerProps> = ({
-  startTime,
-  endTime,
-  timeInterval,
+  slots,
   selected,
   onSelect,
 }) => {
-  const generateTimeSlots = () => {
-    const timeSlots = [];
-    let currentTime = new Date(`2023-01-01 ${startTime}`);
-    const endTimeObj = new Date(`2023-01-01 ${endTime}`);
+  const convertTo12Hour = (time: string) => {
+    const [hours, minutes] = [time.slice(0, 2), time.slice(2, 4)];
+    const hoursInt = parseInt(hours);
+    const minutesInt = parseInt(minutes);
 
-    while (currentTime <= endTimeObj) {
-      timeSlots.push(
-        currentTime.toLocaleTimeString("en-IN", {
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: true,
-        })
-      );
-      currentTime.setMinutes(currentTime.getMinutes() + timeInterval);
+    if (hoursInt > 12) {
+      return `${hoursInt - 12}:${minutesInt.toString().padEnd(2, "0")} PM`;
+    } else if (hoursInt === 12) {
+      return `${hoursInt}:${minutesInt.toString().padEnd(2, "0")} PM`;
+    } else {
+      return `${hoursInt}:${minutesInt.toString().padEnd(2, "0")} AM`;
     }
-
-    return timeSlots;
-  };
-
-  // Handle time slot selection
-  const handleTimeSlotSelect = (time: string) => {
-    onSelect(time);
-    // You can do something with the selected time, like triggering an action or updating state.
-  };
+  }
 
   return (
     <div className="flex flex-col mt-5 gap-2">
       <p className="text-lg font-bold">Select a time slot</p>
-      <ScrollArea className="h-[200px]">
+      <ScrollArea className="h-[100px]">
         <ul className="grid grid-cols-2 gap-3 mt-2">
-          {generateTimeSlots().map((time) => (
+          {slots.map(convertTo12Hour).map((time) => (
             <li
               key={time}
-              onClick={() => handleTimeSlotSelect(time)}
+              onClick={() => onSelect(time)}
               className={`w-5/6 mx-auto p-2 rounded-lg text-center cursor-pointer ${selected === time ? "bg-black text-white" : "bg-gray-200"}`}
             >
               {time}
--- web/src/types/index.ts
+++ web/src/types/index.ts
@@ -1,7 +1,33 @@
 type User = {
+  name: string;
+  username: string;
   email: string;
   given_name: string;
   family_name: string;
   picture: string;
   createdAt: string;
+}
+
+type DayAvailability = {
+  enabled: boolean;
+  slots: string[];
+}
+
+type Service = {
+  enabled: boolean;
+  amount: number;
+  timeslot: string;
+  description: string;
+  name: string;
+  id: string;
+  next_available: string;
+  availability: {
+    "sun" : DayAvailability;
+    "mon" : DayAvailability;
+    "tue" : DayAvailability;
+    "wed" : DayAvailability;
+    "thur" : DayAvailability;
+    "fri" : DayAvailability;
+    "sat" : DayAvailability;
+  };
 }
