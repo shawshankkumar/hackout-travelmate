f7af4ad543bb26e3cbf54ec6106c7ae0c16e7e9d
flying-solo <flyingsoloharshit@gmail.com>
2023-11-05 00:51:12 +0530

    Add new components and dependencies

--- web/src/components/GetSection.tsx
+++ web/src/components/GetSection.tsx
@@ -1,4 +1,5 @@
 import Booking from "@/components/Booking";
+import Calendar from "@/components/Calendar";
 import Profile from "@/components/Profile";
 import { dashboardSectionType, dashboardSections } from "@/utils/constants";
 
@@ -15,7 +16,7 @@ export default function GetSection({
     case "services":
       return <div>services section</div>;
     case "calendar":
-      return <div>calendar section</div>;
+      return <Calendar />;
     case "payments":
       return <div>payments section</div>;
     default:
